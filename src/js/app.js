87b0888cb69d0d7472963168e2975c58929a3abd
bclight2811 <bboy.stenli@yandex.ru>
2022-06-27 23:21:30 +0300

    add header menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,76 @@
 import resize from "./utils/resize";
 import debounce from "./utils/debounce";
+import Swiper from "swiper";
 
 
 resize();
 window.addEventListener("resize", debounce(resize));
+
+const swiper = new Swiper('#main_swiper', {
+  speed: 400,
+  spaceBetween: 50,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+});
+
+const thumbsSwiper = new Swiper('#swiper_thumbs', {
+  speed: 400,
+  spaceBetween: 24,
+  slidesPerView: 4,
+});
+
+
+const modalSwiper = new Swiper('#modal_swiper', {
+  speed: 400,
+  spaceBetween: 50,
+  thumbs: {
+    swiper: thumbsSwiper
+  }
+});
+
+function modalChangeSlide(slide) {
+  modalSwiper.slideTo(slide)
+}
+
+
+function openModal(id) {
+  const modal = document.getElementById(id);
+  if (modal) {
+    document.body.classList.add('no-scroll');
+    modal.classList.add('active');
+  }
+}
+
+
+function closeModal(id) {
+  const modal = document.getElementById(id);
+  if (modal) {
+    document.body.classList.remove('no-scroll');
+    modal.classList.remove('active');
+  }
+}
+
+function modalButtonToggle(button, id) {
+  const buttons = document.querySelectorAll('#product_modal_' + id + ' .modal__inner-info-content-size-btn');
+  buttons.forEach(b => b.classList.remove('active'));
+  button.classList.add('active');
+
+  const price = document.querySelector('#product_modal_' + id + ' [data-bind-price]');
+  price.textContent = button.dataset.price
+
+}
+
+
+function toggleMenu() {
+  const header = document.getElementById('header');
+  header.classList.toggle('active');
+  document.body.classList.toggle('no-scroll');
+}
+
+window.openModal = openModal
+window.closeModal = closeModal
+window.modalChangeSlide = modalChangeSlide
+window.modalButtonToggle = modalButtonToggle
+window.toggleMenu = toggleMenu
